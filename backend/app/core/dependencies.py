3296a0c1f8892f558589e191a69149fdbbedee22
yesinkim <bailando.ys@gmail.com>
2025-05-12 23:24:21 +0900

    refator: metaword, auth, login, board등 사용하지 않는 api 및 구조 삭제

--- backend/app/core/dependencies.py
+++ backend/app/core/dependencies.py
@@ -3,8 +3,3 @@
 """
 from typing import Annotated
 from fastapi import Depends
-from fastapi.security import OAuth2PasswordBearer
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-OAuth2SchemeDepends = Annotated[str, Depends(oauth2_scheme)]
