a635fce7e901f486435cff467cd6dc2b94fc1c7c
skkim <seungkyukim.dev@gmail.com>
2025-05-12 23:33:56 +0900

    chore: change to original code

--- frontend/src/config/unlockConfig.ts
+++ frontend/src/config/unlockConfig.ts
@@ -1,6 +1,6 @@
 export const unlockConfig = {
 	// unlockTime: new Date("2025-05-17T15:00:00+09:00").getTime(), // 배포 시 이 시간 사용
-	unlockTime: new Date("2025-05-11T15:00:00+09:00").getTime(),
-	keywordCount: 15,
+	unlockTime: new Date("2025-05-12T23:33:00+09:00").getTime(),
+	keywordCount: 3,
 	bingoMissionCount: 3,
 };
