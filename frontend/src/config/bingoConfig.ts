35bb1b43f03f168cfea87896f5c7abab1d178e81
skkim <seungkyukim.dev@gmail.com>
2025-05-14 22:51:12 +0900

    feat: change config file name & add end time

--- frontend/src/config/bingoConfig.ts
+++ frontend/src/config/bingoConfig.ts
@@ -1,6 +1,7 @@
-export const unlockConfig = {
+export const bingoConfig = {
 	// unlockTime: new Date("2025-05-17T15:00:00+09:00").getTime(), // 배포 시 이 시간 사용
 	unlockTime: new Date("2025-05-12T23:33:00+09:00").getTime(),
+	conferenceEndTime: new Date("2025-05-14T22:40:00+09:00").getTime(),
 	keywordCount: 3,
 	bingoMissionCount: 3,
 };
