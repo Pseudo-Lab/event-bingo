7ad54946595ddd3eb97c310ef6a561da1ad18e70
skkim <seungkyukim.dev@gmail.com>
2025-05-15 01:14:15 +0900

    chore: revert code

--- backend/app/integrations/omoh/verification.py
+++ backend/app/integrations/omoh/verification.py
@@ -14,7 +14,7 @@ def verify_email_in_attendances(email: str) -> str:
     if not file_path:
         raise ValueError("GUEST_FILE_PATH 환경 변수가 설정되지 않았습니다.")
 
-    # attendance_list = pd.read_excel(file_path)
-    # if email not in attendance_list["Email"].values:
-    raise ValueError(f"수도콘 행사(우모) 신청시 사용한 이메일을 입력해주세요.")
+    attendance_list = pd.read_excel(file_path)
+    if email not in attendance_list["Email"].values:
+        raise ValueError(f"수도콘 행사(우모) 신청시 사용한 이메일을 입력해주세요.")
     return attendance_list.loc[attendance_list["Email"] == email, "Name"].values[0]
