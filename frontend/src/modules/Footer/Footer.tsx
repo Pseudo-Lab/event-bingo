f695472f1b6cd8a7c84ff1c131a72cfefaf19ce2
skkim <seungkyukim.dev@gmail.com>
2025-07-07 23:14:37 +0900

    feat: add example logo

--- frontend/src/modules/Footer/Footer.tsx
+++ frontend/src/modules/Footer/Footer.tsx
@@ -17,13 +17,13 @@ const Footer = () => {
         <Grid container spacing={1}>
           <Grid item xs={12} sm={12} md={12}>
             <CustomLogo
-              maxWidth="100px"
+              maxWidth="30px"
               height="auto"
               marginRight="8px"
             />
           </Grid>
         </Grid>
-        <Box mt={3}>
+        <Box mt={2}>
           <Typography variant="body2" color="inherit" align="center">
             Copyright © 2024 가짜연구소 DevFactory
           </Typography>
