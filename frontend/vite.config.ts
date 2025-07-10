f52ab9dd771bf4ef632768cc2049dcab4687e034
skkim <seungkyukim.dev@gmail.com>
2025-07-10 23:55:56 +0900

    feat: change base url config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: './', 
+  base: './business-experimentation2025', 
   plugins: [react()]
 })
