0a4d3bfcd467c46ae54880737225c9d531e18e5e
yesinkim <bailando.ys@gmail.com>
2025-07-12 01:08:43 +0900

    Merge pull request #76 from Pseudo-Lab/feat/admin
    feat: 관리자용 참여자 및 빙고 보드 데이터 다운로드 API 추가

--- backend/app/api/admin/services.py
+++ backend/app/api/admin/services.py
@@ -1,7 +1,9 @@
 from sqlalchemy.orm.attributes import flag_modified
 from sqlalchemy import select
 from core.db import AsyncSession
+from models.user import BingoUser
 from models.bingo.bingo_boards import BingoBoards
+from typing import List
 
 
 async def set_test_bingo_board(db: AsyncSession, user_id: int, bingo_count: int) -> BingoBoards:
@@ -18,21 +20,17 @@ async def set_test_bingo_board(db: AsyncSession, user_id: int, bingo_count: int)
 
     data = board.board_data
 
-    # 빙고판 크기 동적 계산
-    num_cells = len(data)
-    row_size = int(num_cells ** 0.5)
-
     # 2. 빙고판 초기화 및 재설정
     # 모든 status를 0으로 초기화
-    for i in range(num_cells):
+    for i in range(25):
         if str(i) in data:
             data[str(i)]["status"] = 0
 
     # bingo_count만큼 가로줄 완성
     if bingo_count > 0:
         for i in range(bingo_count):
-            for j in range(row_size):
-                idx = i * row_size + j
+            for j in range(5):
+                idx = i * 5 + j
                 if str(idx) in data:
                     data[str(idx)]["status"] = 1
 
@@ -44,3 +42,21 @@ async def set_test_bingo_board(db: AsyncSession, user_id: int, bingo_count: int)
     await db.refresh(board)
 
     return board
+
+
+async def get_all_users(db: AsyncSession) -> List[BingoUser]:
+    """
+    모든 유저 정보를 조회합니다.
+    """
+    result = await db.execute(select(BingoUser))
+    users = result.scalars().all()
+    return users
+
+
+async def get_all_bingo_boards(db: AsyncSession) -> List[BingoBoards]:
+    """
+    모든 유저의 빙고 보드 정보를 조회합니다.
+    """
+    result = await db.execute(select(BingoBoards))
+    boards = result.scalars().all()
+    return boards
--- backend/app/core/dependencies.py
+++ backend/app/core/dependencies.py
@@ -2,4 +2,26 @@
 공용 Depends 모음
 """
 from typing import Annotated
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from core.db import db
+import os
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+
+AsyncSessionDepends = Annotated[AsyncSession, Depends(db.get_session)]
+
+
+# Swagger/Admin Auth
+SWAGGER_USERNAME = os.environ.get("SWAGGER_USERNAME", "admin")
+SWAGGER_PASSWORD = os.environ.get("SWAGGER_PASSWORD", "admin")
+
+security = HTTPBasic()
+
+def authenticate_user(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    if credentials.username != SWAGGER_USERNAME or credentials.password != SWAGGER_PASSWORD:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect credentials",
+            headers={"WWW-Authenticate": "Basic"},
+        )
