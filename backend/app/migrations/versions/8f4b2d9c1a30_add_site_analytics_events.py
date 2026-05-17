c2ea5c3c15ca827969361615106ab981633d05a6
soohyunme <soohyun.dev@gmail.com>
2026-05-18 08:08:51 +0900

    fix(backend): Alembic migration head 정리

--- backend/app/migrations/versions/8f4b2d9c1a30_add_site_analytics_events.py
+++ backend/app/migrations/versions/8f4b2d9c1a30_add_site_analytics_events.py
@@ -1,7 +1,7 @@
 """add site analytics events table
 
 Revision ID: 8f4b2d9c1a30
-Revises: 3b9c1f0d2a77
+Revises: a1b2c3d4e5f6
 Create Date: 2026-05-18 01:20:00.000000+09:00
 
 """
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 revision: str = "8f4b2d9c1a30"
-down_revision: Union[str, None] = "3b9c1f0d2a77"
+down_revision: Union[str, None] = "a1b2c3d4e5f6"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
